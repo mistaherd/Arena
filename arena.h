cc55caa28c6b41a21d390110cd8df3f7c51f65a1
mistaherd <95090888+mistaherd@users.noreply.github.com>
2026-01-15 01:12:36 +0000

    makeing the libary into stb style

--- arena.h
+++ arena.h
@@ -1,13 +1,50 @@
-#pragma once
+#ifndef ARENA_H
+#define ARENA_H
 #include <stdint.h>
 #include <stddef.h>
-
+#include <stdlib.h>
 typedef struct {
         uint8_t *base;// starting address of arena
         size_t size;
         size_t used;
 }Arena;
+
 Arena *arena_init(size_t size);
 void *arena_alloc(Arena *arena,size_t size);
 void *arena_reset(Arena *arena);
 void *free_arena(Arena *arena);
+#endif // ARENA_H
+#ifdef ARENA_IMPLEMENTATION
+Arena *arena_init(size_t size){
+    Arena *arena=malloc(sizeof(Arena));
+    if (!arena) return NULL;
+    
+    arena->base=malloc(size);
+    if (!arena->base){
+        free(arena);
+        return NULL;
+    }
+    arena->size=size;
+    arena->used=0;
+    return arena;
+}
+
+void *arena_alloc(Arena *arena,size_t size){
+    if (arena->used+size >arena->size){
+        return NULL;
+    }
+    void *ptr=arena ->base +arena->used;
+    arena->used +=size;
+    return ptr;
+}
+void arena_reset(Arena *arena) {
+    arena->used = 0;
+}
+
+// Destroy arena and free all memory
+void free_arena(Arena *arena) {
+    if (!arena) return;
+    free(arena->base);
+    free(arena);
+}
+#endif
